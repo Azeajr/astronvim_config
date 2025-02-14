4ffb397ffbe8c5085eed2b72af92b96dcbd82978
Antonio Zea Jr. <zeajantonio@gmail.com>
2025-02-14 09:29:25 -0500

    Add venv-selector.nvim and update Python MyPy configuration

--- lua/astrocommunity/pack/python-mypy/init.lua
+++ lua/astrocommunity/pack/python-mypy/init.lua
@@ -1,4 +1,4 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
+if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
 return {
   { import = "astrocommunity.pack.python-ruff" }, -- Ensures existing Python tools are included
