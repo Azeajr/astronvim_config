c17253b1b55ce35f6140c1ea1c431e29d060ab0a
Azeajr <antonio.zea.jr@zsuitetech.com>
2025-03-27 23:03:10 -0400

    Initial commit

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,11 +1,13 @@
 if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 
+-- Customize None-ls sources
+
 ---@type LazySpec
 return {
   "nvimtools/none-ls.nvim",
   opts = function(_, opts)
     -- opts variable is the default configuration table for the setup function call
-    local null_ls = require "null-ls"
+    -- local null_ls = require "null-ls"
 
     -- Check supported formatters and linters
     -- https://github.com/nvimtools/none-ls.nvim/tree/main/lua/null-ls/builtins/formatting
@@ -14,13 +16,9 @@ return {
     -- Only insert new sources, do not replace the existing ones
     -- (If you wish to replace, use `opts.sources = {}` instead of the `list_insert_unique` function)
     opts.sources = require("astrocore").list_insert_unique(opts.sources, {
-      -- Add mypy as a diagnostic tool
-      null_ls.builtins.diagnostics.mypy.with {
-        extra_args = function()
-          local virtual_env = os.getenv "VIRTUAL_ENV" or os.getenv "CONDA_PREFIX" or "/usr"
-          return { "--python-executable", virtual_env .. "/bin/python3" }
-        end,
-      },
+      -- Set a formatter
+      -- null_ls.builtins.formatting.stylua,
+      -- null_ls.builtins.formatting.prettier,
     })
   end,
 }
